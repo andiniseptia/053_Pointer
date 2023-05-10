a71596bdd1876c127257ca75f9e0483f7ded8b11
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-10 11:02:05 +0700

    Menulliskan object reference

--- 053_Pointer/053_Pointer.cpp
+++ 053_Pointer/053_Pointer.cpp
@@ -16,5 +16,8 @@ int main()
 	mahasiswa mhs{ 1 };			//Object mhs
 	mhs.showNim();				//Member Access Operator
 
+	mahasiswa& ref = mhs;		//Pointer Reference refMhs
+	ref.nim = 2;				//Member Access Operator 
+	mhs.showNim();
 
 }
