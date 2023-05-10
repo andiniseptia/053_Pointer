b2b316edd8e3e60e2a5f07e0bb7e65a9c6d78f36
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-10 11:34:38 +0700

    Menambahkan fungsi main

--- 053_FriendClass/053_FriendClass.cpp
+++ 053_FriendClass/053_FriendClass.cpp
@@ -22,3 +22,23 @@ public:
 void siswa::displayAll(orang& a) {
 	cout << id << endl << a.nama;
 }
+
+void siswa::setId(int pId) {
+	id = pId;
+}
+
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+int main()
+{
+	orang o;
+	o.setNama("Joko Kumat");
+	siswa s;
+	s.setId(1);
+	s.displayAll(o);
+	cout << endl;
+	system("pause");
+	return 0;
+}
